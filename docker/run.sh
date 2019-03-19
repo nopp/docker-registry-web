7b22527812580a3cbb24c4ebd58137b3002cf63a
Carlos Augusto Malucelli <malucellicarlos@gmail.com>
2019-03-19 14:28:55 -0300

    Dockerfile fix

--- docker/run.sh
+++ docker/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sed -i s/xxx/"$title"/ /etc/registry-ui/config.json
-sed -i s/yyy/"$registryurl"/ /etc/registry-ui/config.json
+sed -i "s|yyy|$registryurl|" /etc/registry-ui/config.json
 
 /usr/bin/python3 drweb.py
